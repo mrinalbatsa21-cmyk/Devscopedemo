f0dc2a6e0463bfbf0aabb97384f0a72bb8bb5fe7
Mrinal Batsa <dev@devscope.com>
2026-05-19 15:18:58 +0530

    DS-1: initial Flask app setup

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+    return "DevScope sample app is running"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
